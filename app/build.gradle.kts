69e7373241f964d569234567e37e256cd7bee920
roddur99 <73806645+roddur99@users.noreply.github.com>
2026-09-04 11:19:53 -0400

    Prepare v0.1.4 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ fun runCommand(command: String): String {
 }
 
 @Suppress("PropertyName")
-val _versionName = "0.1.3"
+val _versionName = "0.1.4"
 val betaCount by lazy {
     val betaTags = runCommand("git tag -l --sort=refname v${_versionName}-b*")
 
@@ -58,7 +58,7 @@ val keystoreProperties = Properties().apply {
 android {
     defaultConfig {
         applicationId = "com.rodro.yokaikomganion"
-        versionCode = 4
+        versionCode = 5
         versionName = _versionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
